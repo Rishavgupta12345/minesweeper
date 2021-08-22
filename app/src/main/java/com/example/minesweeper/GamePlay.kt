5e64b681179415dc11d557001c3f06cacc3f660b
rishavgupta <rishavgupta933@gmail.com>
2021-08-22 10:29:20 +0530

    step 4

--- app/src/main/java/com/example/minesweeper/GamePlay.kt
+++ app/src/main/java/com/example/minesweeper/GamePlay.kt
@@ -5,12 +5,16 @@ import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.board.*
+import kotlin.random.Random
 
 class GamePlay: AppCompatActivity() {
 
     var choice : Int = 1
+    var status  = Status.ONGOING
+    var flaggedMines = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +50,12 @@ class GamePlay: AppCompatActivity() {
     private fun SetUpBoard (row: Int, col: Int, mine: Int)
     {
 
+        // Setting up total number of mines
+        mine_count.text = ""+mine
+
+        // Array of buttons to find the position of a particular button
+        val cellBoard = Array(row) { Array(col) {MineCell(this)}}
+
         action_click.setOnClickListener{
             if(choice==1) {
                 action_click.setImageResource(R.drawable.flag)
@@ -58,6 +68,7 @@ class GamePlay: AppCompatActivity() {
 
         //set the layout
         var counter = 1
+        var isFirstClick = true
 
         val params1 = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.MATCH_PARENT,0
@@ -76,12 +87,35 @@ class GamePlay: AppCompatActivity() {
             for (j in 0 until col) {
 
                 // makeing of button
-                val button = Button(this)
+                val button = MineCell(this)
+
+                //Buttons are being stored to their corresponding locations in the array
+                cellBoard[i][j] = button
+
                 button.id = counter
                 button.textSize = 18.0F
                 button.layoutParams = params2
                 params2.weight = 1.0F
                 button.setBackgroundResource(R.drawable.button)
+
+                button.setOnClickListener{
+
+                    // Checking for first click
+                    if(isFirstClick){
+                        isFirstClick = false
+
+                        // Setting up mines
+                        setMines(i,j,mine,cellBoard,row,col)
+
+                        //Start Timer
+                        //startTimer()
+
+                    }
+
+                    move(choice, i, j, cellBoard, row, col,mine)
+                    display(cellBoard)
+
+                }
 
                 linearLayout.addView(button)
                 counter++
@@ -90,6 +124,128 @@ class GamePlay: AppCompatActivity() {
         }
 
     }
+
+    // Move function
+    private fun move(choice: Int, x: Int, y:Int, cellBoard:Array<Array<MineCell>>, rowSize: Int,colSize: Int,mine:Int): Boolean{
+
+        if(choice==1){
+            if(cellBoard[x][y].isMarked || cellBoard[x][y].isRevealed){
+                return false
+            }
+            if(cellBoard[x][y].value == MINE){
+                status = Status.LOST;
+                //updateScore()
+                return true
+            }
+            else if(cellBoard[x][y].value >0){
+                cellBoard[x][y].isRevealed = true
+                checkStatus(cellBoard,rowSize,colSize);
+                return true
+            }
+            else if(cellBoard[x][y].value==0){
+                handleZero(x,y,cellBoard,rowSize,colSize)
+                checkStatus(cellBoard,rowSize,colSize);
+                return true
+            }
+
+        }
+        if(choice == 2){
+
+            if(cellBoard[x][y].isRevealed) return false
+
+            else if(cellBoard[x][y].isMarked){
+                flaggedMines--
+                cellBoard[x][y].setBackgroundResource(R.drawable.button)
+                cellBoard[x][y].isMarked = false
+                checkStatus(cellBoard,rowSize,colSize)
+            }
+            else {
+                if(flaggedMines==mine){
+                    Toast.makeText(this,"You cannot mark more than $mine mines", Toast.LENGTH_LONG).show()
+                    return false
+                }
+                flaggedMines++
+                cellBoard[x][y].isMarked = true;
+                checkStatus(cellBoard, rowSize, colSize)
+            }
+            var finalMineCount = mine-flaggedMines
+            mine_count.text = ""+finalMineCount
+            return true;
+        }
+
+        return false
+    }
+
+    // Handles when board[x][y]==0
+    private val xDir = intArrayOf(-1, -1, 0, 1, 1, 1, 0, -1)
+    private val yDir = intArrayOf(0, 1, 1, 1, 0, -1, -1, -1)
+    private fun handleZero(x:Int ,y:Int, cellBoard:Array<Array<MineCell>>, rowSize: Int,colSize: Int){
+
+        cellBoard[x][y].isRevealed = true
+        for(i in 0..7){
+            var xstep = x+xDir[i]
+            var ystep = y+yDir[i]
+            if((xstep<0 || xstep>=rowSize) || (ystep<0 || ystep>=colSize)){
+                continue;
+            }
+            if(cellBoard[xstep][ystep].value>0 && !cellBoard[xstep][ystep].isMarked){
+                cellBoard[xstep][ystep].isRevealed = true
+            }else if( !cellBoard[xstep][ystep].isRevealed && !cellBoard[xstep][ystep].isMarked && cellBoard[xstep][ystep].value==0){
+                handleZero(xstep,ystep,cellBoard,rowSize,colSize)
+
+            }
+        }
+
+    }
+
+    private fun setMines(row:Int, col:Int, mine:Int, cellBoard:Array<Array<MineCell>>,rowSize:Int, colSize:Int) {
+        //Generate random coordinates to set mine
+        var mineCount = mine
+        var i=1
+        while(i<=mineCount){
+            var r = (Random(System.nanoTime()).nextInt(0, rowSize))
+            var c = (Random(System.nanoTime()).nextInt(0, colSize))
+            if(r==row || cellBoard[r][c].isMine){
+                continue
+            }
+            cellBoard[r][c].isMine = true
+            cellBoard[r][c].value = -1
+            updateNeighbours(r,c,cellBoard,rowSize,colSize)
+            i++;
+        }
+    }
+
+    // Update the neighbours after setting mine
+    private fun updateNeighbours(row: Int,column: Int,cellBoard: Array<Array<MineCell>>,rowSize:Int,colSize:Int) {
+        for (i in movement) {
+            for (j in movement) {
+                if(((row+i) in 0 until rowSize) && ((column+j) in 0 until colSize) && cellBoard[row+i][column+j].value != MINE)
+                    cellBoard[row+i][column+j].value++
+            }
+        }
+    }
+
+    // To update status (ongoing/won)
+    private fun checkStatus(cellBoard:Array<Array<MineCell>>, rowSize:Int, colSize: Int){
+        var flag1=0
+        var flag2=0
+        for(i in 0 until rowSize){
+            for(j in 0 until colSize){
+                if(cellBoard[i][j].value==MINE && !cellBoard[i][j].isMarked){
+                    flag1=1
+                }
+                if(cellBoard[i][j].value!=MINE && !cellBoard[i][j].isRevealed){
+                    flag2=1
+                }
+            }
+        }
+        if(flag1==0 || flag2==0) status = Status.WON
+        else status = Status.ONGOING
+
+        //if(status==Status.WON) updateScore()
+
+    }
+
 
     private fun gameRestart() {
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
@@ -105,10 +261,8 @@ class GamePlay: AppCompatActivity() {
             startActivity(intent)
         }
 
-        builder.setNegativeButton("No", object : DialogInterface.OnClickListener {
-            override fun onClick(dialog: DialogInterface?, which: Int) {
-            }
-        })
+        builder.setNegativeButton("No"
+        ) { dialog, which -> }
 
         val alertDialog = builder.create()
         alertDialog.show()
@@ -139,4 +293,59 @@ class GamePlay: AppCompatActivity() {
         alertDialog.show()
     }
 
+    // It will display the buttons according to the game status
+    private fun display(cellBoard:Array<Array<MineCell>>) {
+        cellBoard.forEach { row ->
+            row.forEach {
+                if(it.isRevealed)
+                    setNumberImage(it)
+                else if (it.isMarked)
+                    it.setBackgroundResource(R.drawable.flag)
+                else if (status == Status.LOST && it.value == MINE) {
+                    restart_button.setBackgroundResource(R.drawable.sadhubohot)
+                    it.setBackgroundResource(R.drawable.mine)
+                }
+                //To show that mine is not present here but it is marked
+//                if(status == Status.LOST && it.isMarked && !it.isMine){
+//                    it.setBackgroundResource(R.drawable.crossedflag)
+//                }
+                else if (status == Status.WON && it.value == MINE) {
+                    it.setBackgroundResource(R.drawable.flag)
+                    restart_button.setBackgroundResource(R.drawable.happy)
+                }
+                else
+                    it.text = " "
+            }
+
+        }
+    }
+
+    // This function will display images according to status
+    // Game status is checked in display function (Called from display function)
+    private fun setNumberImage(button:MineCell) {
+        if(button.value==0) button.setBackgroundResource(R.drawable.zero)
+        if(button.value==1) button.setBackgroundResource(R.drawable.one)
+        if(button.value==2) button.setBackgroundResource(R.drawable.two)
+        if(button.value==3) button.setBackgroundResource(R.drawable.three)
+        if(button.value==4) button.setBackgroundResource(R.drawable.four)
+        if(button.value==5) button.setBackgroundResource(R.drawable.five)
+        if(button.value==6) button.setBackgroundResource(R.drawable.six)
+        if(button.value==7) button.setBackgroundResource(R.drawable.seven)
+        if(button.value==8) button.setBackgroundResource(R.drawable.eights)
+
+    }
+
+
+    companion object{
+        const val MINE = -1
+        val movement = intArrayOf(-1, 0, 1)
+    }
+
+}
+
+// Includes all the three game status
+enum class Status{
+    WON,
+    ONGOING,
+    LOST
 }
